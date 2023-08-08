0afc29a60a4b6e20fa0f75b6faad00c523c70956
srinathprovab <srinathprovab@gmail.com>
2023-08-08 13:59:55 +0530

    first commit

---
+++ Beeoons/APP/Config/UIColors/ColorManager.swift
@@ -0,0 +1,83 @@
+//
+//  ColorManager.swift
+//  DoorcastRebase
+//
+//  Created by Codebele 07 on 04/05/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    
+    public static var ButtonColor : UIColor {
+        
+        get {
+            return UIColor(named: "ButtonColor")!
+        }
+    }
+    
+    public static var CalenderRangeColor : UIColor {
+        
+        get {
+            return UIColor(named: "CalenderRangeColor")!
+        }
+    }
+    
+    
+    public static var NavBackColor : UIColor {
+        
+        get {
+            return UIColor(named: "NavBackColor")!
+        }
+    }
+    
+    public static var PriceBoxColor : UIColor {
+        
+        get {
+            return UIColor(named: "PriceBoxColor")!
+        }
+    }
+    
+    
+    public static var SubtitleColor : UIColor {
+        
+        get {
+            return UIColor(named: "SubtitleColor")!
+        }
+    }
+    
+    public static var TitleColor : UIColor {
+        
+        get {
+            return UIColor(named: "TitleColor")!
+        }
+    }
+    
+    
+    public static var UnselectedBtnColor : UIColor {
+        
+        get {
+            return UIColor(named: "UnselectedBtnColor")!
+        }
+    }
+    
+    public static var WhiteColor : UIColor {
+        
+        get {
+            return UIColor(named: "WhiteColor")!
+        }
+    }
+    
+    
+    public static var AppBorderColor : UIColor {
+        
+        get {
+            return UIColor(named: "AppBorderColor")!
+        }
+    }
+    
+    
+    
+}
